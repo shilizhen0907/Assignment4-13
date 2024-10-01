cd7725eb6bb46f550378e96509a21e4f0fdbc2b1
Lizhen Shi <lshi5@hawkmail.hccfl.edu>
2024-10-01 16:29:08 -0400

    test

--- CheckVowelOrConsonant.java
+++ CheckVowelOrConsonant.java
@@ -8,13 +8,15 @@ public class CheckVowelOrConsonant {
         System.out.print("Enter a letter: ");
         String s = input.nextLine();
 
+        //test
+
         //check if the user enters a letter or more
         if (s.length() == 1) {
             //read a character from consele
             char ch = s.charAt(0);  
             // check if the entered character is a letter 
             if (Character.isLetter(ch)) {
-                /*if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){ */
+            /* if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')){ */
                     //check if the letter is vowels
                     if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' ||
                         ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
